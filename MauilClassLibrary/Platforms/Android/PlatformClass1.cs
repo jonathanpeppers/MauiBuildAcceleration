5408f15f4ce3a4871f2de37ee4b3572547b9d72e
Jonathan Peppers <jonathan.peppers@microsoft.com>
2024-01-04 09:16:14 -0600

    Add 1 AndroidResource

--- MauilClassLibrary/Platforms/Android/PlatformClass1.cs
+++ MauilClassLibrary/Platforms/Android/PlatformClass1.cs
@@ -3,6 +3,6 @@
     // All the code in this file is only included on Android.
     public class PlatformClass1
     {
-        public static string Platform => "Android";
+        public static string Platform => Android.App.Application.Context.GetString(Resource.String.myplatform);
     }
 }
